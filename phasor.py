8432f7986079be78c6aec9fcd39197181246b0de
Boris Alguliev <boris.alguliev@gmail.com>
2018-04-20 00:52:05 -0700

    Working prototype

--- phasor.py
+++ phasor.py
@@ -2,119 +2,121 @@ from vpython import *
 
 
 def vec(x: float, y: float) -> vector:
-    return vector(x, y, 0)  # Make everything easier and confine to 2 dimensions
+    return vector(x, y, 0)  # Simplify and confine to 2 dimensions
 
 
 class Phasor:
-    def __init__(self, magnitude: float, angle: radians, omega, visible=True):
+    def __init__(self, magnitude: float, angle: radians, omega, color=color.white, visible=True):
         self._arrow = arrow(length=magnitude, axis=vec(1, 0).rotate(angle),
-                            shaftwidth=0.1, headwidth=0.1)
+                            shaftwidth=0.1, headwidth=0.1, color=color)
         self.omega = omega
         self.visible = visible
 
     @classmethod
-    def from_vector(cls, v: vector, visible=True):
-        phasor = cls(0, 0, 0)
-        phasor.visible = visible
+    def from_vector(cls, v: vector, omega, color=color.white, visible=True) -> 'Phasor':
+        phasor = cls(0, 0, omega, color, visible)
         phasor._arrow.axis = v
         return phasor
 
     @classmethod
-    def from_components(cls, x: float, y: float, visible=True):
-        return cls.from_vector(vec(x, y), visible)
+    def from_components(cls, x: float, y: float, omega, color=color.white, visible=True) -> 'Phasor':
+        return cls.from_vector(vec(x, y), omega, color, visible)
 
     @property
-    def axis(self):
+    def axis(self) -> vector:
         return self._arrow.axis
-
     @axis.setter
     def axis(self, value):
         self._arrow.axis = value
 
     @property
-    def visible(self):
+    def visible(self) -> bool:
         return self._arrow.visible
-
     @visible.setter
     def visible(self, value):
         self._arrow.visible = value
 
+    @property
+    def color(self) -> vector:
+        return self._arrow.color
+    @color.setter
+    def color(self, value: vector):
+        self._arrow.color = value
+
+    @property
+    def magnitude(self) -> float:
+        return self.axis.mag
+
+    @property
+    def pos(self) -> vector:
+        return self._arrow.pos
+
+    @property
+    def angle(self) -> radians:
+        return diff_angle(vec(1, 0), self.axis)
+
+    def rotate(self, angle: radians):
+        self._arrow.rotate(angle=angle, axis=vector(0, 0, 1))
+
     def add_to_tip(self, other: 'Phasor') -> 'Phasor':
         other._arrow.pos = self.axis + self._arrow.pos
-        other.axis = self.axis + other.axis
         return other
-        # phasor = Phasor.from_vector(self.axis + other.axis)
-        # phasor._arrow.pos = self.axis + self._arrow.pos
-        # return phasor
 
     def __add__(self, other):
         self.add_to_tip(other)
 
-    def rotate(self, angle: radians):
-        self._arrow.rotate(angle=angle, axis=vector(0, 0, 1))
+    def __str__(self):
+        return str(self.axis)
 
 
 class PhasorDiagram:
     dt = 0.01
 
     def __init__(self):
-        self._canvas = canvas()
+        self.run = False
+        self.phasors = list()
+        self._canvas = canvas(width=600, height=600, align='left')
         self._canvas.autoscale = False
+        self._canvas.userspin = False
+        self._canvas.select()
         self._xaxis = box(length=100, width=0.01, height=0.01, canvas=self._canvas)
         self._yaxis = box(length=100, width=0.01, height=0.01, axis=vec(0, 1), canvas=self._canvas)
-        self.phasors = list()
-        self.result = Phasor(0, 0, 0, False)
+
+        self.last_added = Phasor(0, 0, 0)
+        self.result = Phasor(0, 0, 0, color.green)
+        self.y_proj = Phasor(0, 0, 0, color.yellow)
+
+        self.t = 0
+        self.graph = gcurve(color=vector(255, 197, 0), interval=3, graph=graph(background=color.black, align='right'))
 
     def add(self, phasor: Phasor):
-        if self.phasors:  # If there are phasors already, hide additional ones
-            phasor.visible = False
-        else:  # We must see at least one, however
-            phasor.visible = True
-        self.result.add_to_tip(phasor)
         self.phasors.append(phasor)
 
     def cycle(self):
-        result = Phasor(0, 0, 0, False)
+        self.last_added = Phasor(0, 0, 0)
+        # self.result.axis = vec(0, 0)
+        # self.last_added.axis = self.last_added.pos = vec(0, 0)
         for phasor in self.phasors:
             phasor.rotate(phasor.omega * dt)
-            self.result.add_to_tip(phasor)
+            self.last_added = self.last_added.add_to_tip(phasor)
+
+        self.t += dt
+        self.result.axis = self.last_added.pos + self.last_added.axis
+        y_projection = self.result.axis.y
+        self.y_proj.axis = vec(0, y_projection)
+        self.graph.plot(self.t, y_projection)
 
 
-# p1 = Phasor.from_vector(vec(-1, 1))
-# p2 = Phasor.from_components(1, 0, False)
-# p3 = Phasor.from_components(1, 0, False)
-# p4 = Phasor.from_components(1, 0, False)
-# p5 = Phasor.from_components(1, 0, False)
 PD = PhasorDiagram()
 
-PD.add(Phasor.from_vector(vec(-1, 1)))
-PD.add(Phasor.from_components(1, 0))
-PD.add(Phasor.from_components(1, 0))
-PD.add(Phasor.from_components(1, 0))
-PD.add(Phasor.from_components(1, 0))
-
-# print('p1', p1)
-# print(p1.axis)
-# print(p2.axis)
-# p1.axis = vec(-0.5, 0.5)
-# print(p1.axis)
-#
-#
-# pf = p1.add_to_tip(p2).add_to_tip(p3).add_to_tip(p4).add_to_tip(p5)
-# print(pf.axis)
+PD.add(Phasor.from_vector(vec(-1, 1), 1))
+PD.add(Phasor.from_components(1, 0, 2))
+PD.add(Phasor.from_components(1, 1, 3))
+PD.add(Phasor.from_components(1, 0.5, -1.5))
+# PD.add(Phasor.from_components(0, 0.3, 1))
 
 t = 0
 dt = 0.005
 while True:
-    rate(50)
+    rate(30)
     PD.cycle()
-    # pf.rotate(0.01)
-
-    # t += dt
-    # print("Rotating", t)
-# print(type(p3))
-# print('p3', p3)
-# Phasor(0.5, pi/3)
-# Phasor(1.5, pi/4)
-
-# PhasorDiagram()
